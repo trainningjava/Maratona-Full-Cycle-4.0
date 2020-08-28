e5b5c881dc0b167307fe59454590d2e1ba061f0a
trainningjava <48299029+trainningjava@users.noreply.github.com>
2020-08-28 18:55:36 -0300

    Desafio5

--- desafio5/src/app.controller.spec.ts
+++ desafio5/src/app.controller.spec.ts
@@ -15,9 +15,9 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "D E S A F I O 5"', () => {
+    it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe(
-        '<html><title>Maratona Full Cycle 4.0</title><body><br /><br /><br /><br /><center><h1 style="color:blue;">Maratona Full Cycle 4.0</h1></center><br /><br /><center>D E S A F I O 5</center></body></hhtml>',
+        '<html><title>Maratona Full Cycle 4.0</title><body><br /><br /><br /><br /><center><img width="120" height="40" src="https://github.com/trainningjava/Maratona-Full-Cycle-4.0/blob/master/public/assets/images/grupo_4378.png?raw=true"></center><br /><br /><center>D E S A F I O 5</center></body></html>',
       );
     });
   });
